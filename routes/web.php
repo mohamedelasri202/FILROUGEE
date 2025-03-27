951c47752201e63a2f3c9c77872eb74008016b34
mohamedelasri202 <el7994995@gmail.com>
2025-03-27 02:25:17 +0000

    adding the routes for the deleting a product then  some routes to load the view for the admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -40,26 +40,23 @@ Route::post('/register', [UserController::class, 'register'])->name('register.po
 Route::get('/login', [UserController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [UserController::class, 'login'])->name('login.post');
 // showing the dashboards 
-// Route::get('/dashboard/admin', [AdminController::class, 'showAdminDashboard'])->name('dashboard.admin')->middleware('auth', 'role:admin');
-// // the route for the user to get to the home page 
-// Route::get('/dashboard/user', [UserController::class, 'showUserDashboard'])->name('home');
+Route::get('/dashboard/admin', [AdminController::class, 'showAdminDashboard'])->name('dashboard.admin')->middleware('auth', 'role:admin');
+// the route for the user to get to the home page 
+Route::get('/dashboard/user', [UserController::class, 'showUserDashboard'])->name('home');
 
-// Route::get('/dashboard/users', [AdminController::class, 'showUsers'])->name('users')->middleware('auth', 'role:admin');
+Route::get('/dashboard/users', [AdminController::class, 'showUsers'])->name('users')->middleware('auth', 'role:admin');
 
-// // Route::get('/dashboard/vendor', [VendorController::class, 'index'])->name('vendor');
-// Route::post('/logout', [UserController::class, 'logout'])->name('logoutt');
+// Route::get('/dashboard/vendor', [VendorController::class, 'index'])->name('vendor');
+Route::post('/logout', [UserController::class, 'logout'])->name('logoutt');
 
-// // show the page that shows all the products 
+// show the page that shows all the products 
 
-// Route::get('/products', [UserController::class, 'showProducts'])->name('products')->middleware('auth');
 
-// // show the page that shows services
-// Route::get('/services', [UserController::class, 'services'])->name('services');
-// // controle the user status
-// Route::put('/users/status', [UserController::class, 'updateStatus'])->name('users.updateStatus')->middleware('auth', 'role:admin');
+
+
 
 // routes for the  product  routes 
 Route::post('/dashboard/vendor', [ProductController::class, 'addproduct'])->name('addproduct')->middleware('auth', 'role:vendor');
 Route::get('/dashboard/vendor/products', [ProductController::class, 'showProducts'])->name('vendor.products')->middleware('auth', 'role:vendor');
-
+Route::delete('vendor/product/{id}', [ProductController::class, 'deleteproduct'])->name('deleteproduct')->middleware('auth', 'role:vendor');
 Route::put('/dashboard/vendor/{id}', [ProductController::class, 'updateproduct'])->name('updateproduct')->middleware('auth', 'role:vendor');
