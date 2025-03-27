5ccedc3af9c8ad83618a51d092a2841bffcfb1be
mohamedelasri202 <el7994995@gmail.com>
2025-03-27 01:22:43 +0000

    addin and function to add a product by the vendor

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Shoopingcart;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+use App\Repositories\ProductRepository;
+use App\Repositories\ProductRepositoryInterface;
+use App\Repositories\ShoopingcartRepositoryInterface;
+
+class ProductController extends Controller
+
+{
+    protected $ProductRepository;
+
+    public function __construct(ProductRepositoryInterface $ProductRepository, ShoopingcartRepositoryInterface $shoopingcartRepository)
+    {
+
+        $this->ProductRepository = $ProductRepository;
+    }
+
+    public function addproduct(Request $request)
+    {
+        $imagePath = null;
+
+        $request->validate([
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'image' => 'required|image',
+            'price' => 'required|numeric',
+            'category' => 'required|string'
+
+        ]);
+
+
+
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('products', 'public');
+        }
+
+        $this->ProductRepository->addProduct($request, $imagePath);
+
+        return redirect()->route('dashboard.vendor')->with('success', 'Product added successfully!');
+    }
+}
