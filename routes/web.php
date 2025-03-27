fe3679dc395cbe1be86c82e6120ac7cb09650d3c
mohamedelasri202 <el7994995@gmail.com>
2025-03-27 02:18:11 +0000

    adding the routes for the products function

--- routes/web.php
+++ routes/web.php
@@ -35,11 +35,11 @@ Route::get('/', function () {
 
 
 
-// Route::get('/register', [UserController::class, 'showRegistrationForm'])->name('register');
-// Route::post('/register', [UserController::class, 'register'])->name('register.post');
-// Route::get('/login', [UserController::class, 'showLoginForm'])->name('login');
-// Route::post('/login', [UserController::class, 'login'])->name('login.post');
-// // showing the dashboards 
+Route::get('/register', [UserController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [UserController::class, 'register'])->name('register.post');
+Route::get('/login', [UserController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [UserController::class, 'login'])->name('login.post');
+// showing the dashboards 
 // Route::get('/dashboard/admin', [AdminController::class, 'showAdminDashboard'])->name('dashboard.admin')->middleware('auth', 'role:admin');
 // // the route for the user to get to the home page 
 // Route::get('/dashboard/user', [UserController::class, 'showUserDashboard'])->name('home');
@@ -58,50 +58,5 @@ Route::get('/', function () {
 // // controle the user status
 // Route::put('/users/status', [UserController::class, 'updateStatus'])->name('users.updateStatus')->middleware('auth', 'role:admin');
 
-// // routes for adding a product 
-// Route::post('/dashboard/vendor', [ProductController::class, 'addproduct'])->name('addproduct')->middleware('auth', 'role:vendor');
-// Route::get('/dashboard/vendor/products', [ProductController::class, 'showProducts'])->name('vendor.products')->middleware('auth', 'role:vendor');
-
-// // routes for showing the dashboard aand sending data to the view 
-// // Route::get('/dashboard/vendor', [ProductController::class, 'index'])->name('index')->middleware('auth', 'role:vendor');
-
-// Route::get('/dashboard/vendor', [VendorDashboardController::class, 'index'])->name('dashboard.vendor');
-
-// // update the product 
-
-// Route::put('/dashboard/vendor/{id}', [ProductController::class, 'updateproduct'])->name('updateproduct')->middleware('auth', 'role:vendor');
-
-
-// // delete the product
-// Route::delete('vendor/product/{id}', [ProductController::class, 'deleteproduct'])->name('deleteproduct')->middleware('auth', 'role:vendor');
-
-// // *************************************** service provider routes ********************************************
-// Route::get('/dashboard/service_provider', [ServiceController::class, 'index'])->name('dashboard.service_provider')->middleware('auth', 'role:service_provider');
-// Route::post('/addingservice', [ServiceController::class, 'add_service'])->name('add_service');
-// Route::delete('/dashboard/service_provider/services/{id}', [ServiceController::class, 'deleteservice'])->name('deleteservice')->middleware('auth', 'role:service_provider');
-// Route::put('/dashboard/service_provider/services/{id}', [ServiceController::class, 'updateservice'])->name('updateservice')->middleware('auth', 'role:service_provider');
-
-// // ****************************************addind the product to the shooping cart******************************
-
-// Route::post('/user/shoopingcart', [ProductController::class, 'addtoshoopingcart'])->name('addshoopingcart')->middleware('auth', 'role:user');
-
-// // /**************ading the service to service cart  */
-// Route::post('/services', [ServicecartController::class, 'addservicecart'])->name('addservice')->middleware('auth', 'role:user');
-// // ***************** the route for the view of th cart **************************/
-// Route::get('/cart', [CartController::class, 'index'])->name('cart');
-
-// // remove the product from the cart 
-// Route::delete('cart/remove/{id}', [CartController::class, 'removefromcart'])->name('r_product_cart');
-// // updating the quntity of each product in the cart 
-// Route::put('update/quantity/{id}', [CartController::class, 'updat_quantity'])->name('updat_quantity')->middleware('auth', 'role:user');
-// //  the view for the conifermation page 
-// Route::get('confirmation', [OrderController::class, 'checkout'])->name('checkout')->middleware('auth', 'role:user');
-
-// // checkout 
-
-
-// Route::delete('/cart/{id}', [ServicecartController::class, 'deleteservicecart'])
-//     ->name('deletecart')
-//     ->middleware('auth', 'role:user');
-// Route::post('/checkout', [OrderController::class, 'store'])->name('checkout.store');
-// Route::post('/checkout', [OrderController::class, 'add_order'])->name('CHECK')->middleware('auth', 'role:user');
+// routes for the  product  routes 
+Route::post('/dashboard/vendor', [ProductController::class, 'addproduct'])->name('addproduct')->middleware('auth', 'role:vendor');
