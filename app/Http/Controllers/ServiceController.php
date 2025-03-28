98f94e481041b59f9f145732aa26fd1609a53cf7
mohamedelasri202 <el7994995@gmail.com>
2025-03-28 12:10:27 +0000

    adding a service  method by the service provider

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -24,4 +24,27 @@ class ServiceController extends Controller
         $services = $this->ServiceRepository->showALLservices();
         return view('dashboard.service_provider', compact('services'));
     }
+    public function add_service(Request $request)
+    {
+        $imagePath = null;
+
+        $request->validate([
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'image' => 'required|image',
+            'price' => 'required|numeric',
+            'category' => 'required|string'
+
+        ]);
+
+
+
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('products', 'public');
+        }
+
+        $this->ServiceRepository->add_service($request, $imagePath);
+
+        return redirect()->route('dashboard.service_provider')->with('success', 'Product added successfully!');
+    }
 }
