3d4ef4374a6f42e93a242033a16e3cd9f9c23be6
mohamedelasri202 <el7994995@gmail.com>
2025-03-28 12:19:07 +0000

    adding the function to the services interface

--- app/Repositories/ServiceRepositoryInterface.php
+++ app/Repositories/ServiceRepositoryInterface.php
@@ -6,4 +6,7 @@ namespace App\Repositories;
 interface ServiceRepositoryInterface
 {
     public function add_service($data, $imagePath);
+    public function deleteservice($id);
+    public function updateservice($data, $id);
+    public function showALLservices();
 }
