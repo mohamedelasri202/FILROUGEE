83e1100842ef6106028d3c2a629e33715be6aa9b
mohamedelasri202 <el7994995@gmail.com>
2025-03-28 15:50:41 +0000

    adding the shooping cart to the repository

---
+++ app/Repositories/ShoopingcartRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Product;
+use App\Repositories\ShoopingcartRepositoryInterface;
+use App\Models\Shoopingcart;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class ShoopingcartRepository implements ShoopingcartRepositoryInterface
+{
+    public function addToShoopingCart($data)
+    {
+        $productId = $data['product_id'];
+
+        // Check if the product already exists in the user's shopping cart
+        $existing = Shoopingcart::where('product_id', $productId)
+            ->where('user_id', Auth::id())
+            ->first();
+
+        if ($existing) {
+            // If exists, just update the quantity
+            $existing->quantity += 1;
+            $existing->save();
+        } else {
+            // If not, create new entry
+            Shoopingcart::create([
+                'product_id' => $productId,
+                'quantity' => $data['quantity'],
+                'type' => $data['type'],
+                'user_id' => Auth::id()
+            ]);
+        }
+    }
+}
