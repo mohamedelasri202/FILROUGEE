9ca5cb066bd3b88d87b56d065288f4154a86cc81
mohamedelasri202 <el7994995@gmail.com>
2025-03-29 17:21:07 +0000

    adding a service to a cart methode

---
+++ app/Http/Controllers/ServicecartController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories\ServicecartRepositoryInterface;
+use App\Repositories\ServicecartRepository;
+
+class ServicecartController extends Controller
+{
+    protected $ServicecartRepository;
+
+
+    public function __construct(ServicecartRepositoryInterface $ServicecartRepository)
+    {
+        $this->ServicecartRepository = $ServicecartRepository;
+    }
+    public function addservicecart(Request $request)
+    {
+        $services = $request->validate([
+            'service_id' => 'required|integer'
+
+        ]);
+        $this->ServicecartRepository->addservicecart($request);
+        return redirect()->route('services');
+    }
+}
