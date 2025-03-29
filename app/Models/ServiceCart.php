0c4e30f9e01ef4ebe70184bfbae158ec2d2d86b8
mohamedelasri202 <el7994995@gmail.com>
2025-03-29 17:28:33 +0000

    the service cart modul

---
+++ app/Models/ServiceCart.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ServiceCart extends Model
+{
+    use HasFactory;
+    protected $table = 'servicecart';
+    protected $fillable = [
+        'service_id',
+        'user_id'
+    ];
+}
