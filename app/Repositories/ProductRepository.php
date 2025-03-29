9dfba3aeac9d32fd7a51f671b655d95a3d56e91a
mohamedelasri202 <el7994995@gmail.com>
2025-03-29 17:33:11 +0000

    updating the logic for adding deleting and showing all the products

---
+++ app/Repositories/ProductRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Product;
+use App\Models\Shoopingcart;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Repositories\ProductRepositoryInterface;
+
+class ProductRepository implements ProductRepositoryInterface
+{
+    public function addProduct($request, $imagePath)
+    {
+        return Product::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'price' => $request->input('price'),
+            'image' => $imagePath ? 'storage/' . $imagePath : null,
+            'category' => $request->input('category'),
+            'vendor_id' => Auth::id(),
+        ]);
+    }
+    public function showALLproducts()
+    {
+        $procucts = Product::all();
+        return $procucts;
+    }
+    public function updatproduct($data, $id)
+    {
+        $product = Product::findOrFail($id);
+
+        $product->update([
+            'title' => $data['title'],
+            'description' => $data['description'],
+            'price' => $data['price'],
+            'category' => $data['category'],
+        ]);
+    }
+
+    public function deleteproduct($id)
+    {
+        $product = Product::findOrFail($id);
+        $product->delete();
+    }
+}
