ef6401ec48a69222484d92f54948594e892e119d
mohamedelasri202 <el7994995@gmail.com>
2025-03-29 17:24:25 +0000

    the method of counting the services in the cart services

--- app/Repositories/ServicecartRepositoryInterface.php
+++ app/Repositories/ServicecartRepositoryInterface.php
@@ -7,4 +7,5 @@ interface ServicecartRepositoryInterface
     public function addservicecart($request);
     public function showservices();
     public function deleteservicecart($id);
+    public function countservices();
 }
