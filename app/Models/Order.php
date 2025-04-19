2fa6bb907cf02e94c98727fb4c865267b21d2ad4
mohamedelasri202 <el7994995@gmail.com>
2025-04-19 15:24:56 +0100

    creating the model for the order controller

---
+++ app/Models/Order.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'last_name',
+        'email',
+        'phone',
+        'address',
+        'city',
+        'type',
+        'payment_method',
+        'shoopingcart_id',
+        'servicecart_id',
+        'user_id',
+        'quantity',
+        'status',
+        'total'
+    ];
+}
