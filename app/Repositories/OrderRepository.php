eeaef928c0214eb36afcf9991aaa3b8d06c8f080
mohamedelasri202 <el7994995@gmail.com>
2025-04-19 15:21:23 +0100

    adding the add order repository int the repository

---
+++ app/Repositories/OrderRepository.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Order;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+use App\Repositories\OrderRepositoryInterface;
+
+class OrderRepository implements OrderRepositoryInterface
+{
+
+    public function add_order($request)
+    {
+
+        // Add each service
+        if (!empty($request['servicecart_id'])) {
+            foreach ($request['servicecart_id'] as $service_cart) {
+                Order::create([
+                    'name' => $request['name'],
+                    'last_name' => $request['last_name'],
+                    'email' => $request['email'],
+                    'address' => $request['address'],
+                    'city' => $request['city'],
+                    'phone' => $request['phone'],
+                    'payment_method' => $request['payment_method'],
+                    'servicecart_id' => $service_cart,
+                    'type' => $request['type'],
+                    'quantity' => $request['quantity'],
+                    'status' => $request['status'],
+                    'user_id' => Auth::id(),
+                    'total' => $request['total']
+                ]);
+            }
+        }
+
+        // Add only once for the product
+        if (!empty($request['shoopingcart_id'])) {
+            foreach ($request['shoopingcart_id'] as $shooping_id) {
+                Order::create([
+                    'name' => $request['name'],
+                    'last_name' => $request['last_name'],
+                    'email' => $request['email'],
+                    'address' => $request['address'],
+                    'city' => $request['city'],
+                    'phone' => $request['phone'],
+                    'payment_method' => $request['payment_method'],
+                    'shoopingcart_id' => $shooping_id,
+                    'type' => $request['type'],
+                    'quantity' => $request['quantity'],
+                    'status' => $request['status'],
+                    'user_id' => Auth::id(),
+                    'total' => $request['total']
+
+                ]);
+            }
+        }
+    }
+}
