42a24f16c47af5e8d503eea23217d6758de28660
mohamedelasri202 <el7994995@gmail.com>
2025-04-19 15:26:28 +0100

    adding the checkout mthod and passing all tthe data to the view in the method in the controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -11,12 +11,45 @@ use App\Repositories\ShoopingcartRepositoryInterface;
 class OrderController extends Controller
 {
     protected $OrderRepository;
-
+    protected $ServicecartRepository;
+    protected $shoopingcartRepository;
     public function __construct(OrderRepositoryInterface $OrderRepository, ServicecartRepositoryInterface $ServicecartRepository, ShoopingcartRepositoryInterface $shoopingcartRepository)
     {
         $this->OrderRepository = $OrderRepository;
-    }
 
+        $this->ServicecartRepository = $ServicecartRepository;
+        $this->shoopingcartRepository = $shoopingcartRepository;
+    }
+    public function checkout()
+    {
+
+        $services =  $this->ServicecartRepository->showservices();
+        $service_count = $this->ServicecartRepository->countservices();
+        //variables for products now 
+        $products = $this->shoopingcartRepository->showproducts();
+        $product_count = $this->shoopingcartRepository->countproduct();
+        $cart_count =  $service_count +  $product_count;
+        // logic to update the quantity 
+        $totalproduct = 0;
+        foreach ($products as $product) {
+            $totalproduct += $product->quantity * $product->price;
+        }
+        $totalservice = 0;
+        foreach ($services as $service) {
+            $totalservice += $service->price;
+        }
+        // logic to count the total 
+        if ($product_count == 0) {
+            $total = $totalproduct + $totalservice + 1.92 + 5.00;
+        } else {
+            $total = $totalproduct + $totalservice + 1.95 + 5.00;
+        };
+
+
+
+
+        return view('cart.checkout', compact('service_count', 'services', 'products', 'product_count', 'cart_count', 'totalproduct', 'totalservice', 'total'));
+    }
     public function add_order(Request $request)
     {
 
